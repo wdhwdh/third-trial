12e64258877ead2e978ce8c379d5e137eeb7dc56
dh <dh@dh-RV411-RV511-E3511-S3511-RV711-E3411>
2019-05-06 19:14:36 +0900

    initial commit

---
+++ src/thirdtrial.java
@@ -0,0 +1,9 @@
+
+public class thirdtrial {
+public static void main(String[]args) {
+	System.out.println("안녕하세요");
+	System.out.println("이번이 세번째 입니다");
+	System.out.println("조금씩 적응이 되어가네요");
+}
+
+}
