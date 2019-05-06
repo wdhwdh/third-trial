9dcce761237c335f30315362ad45dc74e8f54565
dh <dh@dh-RV411-RV511-E3511-S3511-RV711-E3411>
2019-05-06 19:17:37 +0900

    수정을 해봅니다 메시지 추가

--- src/thirdtrial.java
+++ src/thirdtrial.java
@@ -4,6 +4,7 @@ public static void main(String[]args) {
 	System.out.println("안녕하세요");
 	System.out.println("이번이 세번째 입니다");
 	System.out.println("조금씩 적응이 되어가네요");
+	System.out.println("수정을 해봅니다");
 }
 
 }
